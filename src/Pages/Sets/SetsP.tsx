069886ee90fc3289f3d6136559a7ea161205042a
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2024-03-29 08:37:21 +0100

    Added new page

---
+++ src/Pages/Sets/SetsP.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './SetsP.scss';
+import { SetsPropsM } from "../../Models/Pages/sets-props.model";
+
+export function SetsP(props: SetsPropsM): JSX.Element {
+  return <div className="sets"></div>;
+}
