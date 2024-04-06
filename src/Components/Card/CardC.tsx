7b57f9b211b5c36c9df7f83e4567bb40221919c5
Sebastian-Rosenblad <sebastian.a.rosenblad@gmail.com>
2024-04-06 15:04:20 +0200

    Updated function for checking if text on card should be centered

--- src/Components/Card/CardC.tsx
+++ src/Components/Card/CardC.tsx
@@ -39,12 +39,12 @@ export function CardC(props: CardComponentPropsM): JSX.Element {
   function textShouldBeCentered(): boolean {
     if (card.text) {
       const keywords = [",", "&", "haste", "flying", "first strike", "double strike", "trample", "flash", "vigilance", "lifelink", "reach", "deathtouch", "indestructible", "menace", "hexproof", "shroud", "defender", "protection", "fear", "intimidate", "landwalk", "islandwalk", "forestwalk", "mountainwalk", "swampwalk", "plainswalk", "mill", "scry", "fight", "morph", "megamorph", "surveil", "mutate", "equip", "fortify", "enchant", "aura", "kick", "prowess", "fabricate", "convoke", "explore", "extort", "dredge", "afflict", "amass", "annihilator", "banding", "battle cry", "bestow", "bloodthirst", "bushido", "cascade", "champion", "changeling", "cipher", "clash", "companion", "conspire", "contrive", "crew", "cumulative upkeep", "cycling", "dash", "delve", "devour", "domain", "echo", "embalm", "emerge", "entwine", "epic", "escape", "eternalize", "evoke", "evolve", "exalted", "exploit", "extort", "fading", "flanking", "forecast", "fortell", "fuse", "goad", "graft", "gravestorm", "haunt", "hideaway", "horsemanship", "improvise", "infect", "ingest", "join forces", "level up", "living weapon", "manifest", "meld", "miracle", "monstrosity", "morbid", "myriad", "ninjutsu", "offering", "outlast", "overload", "partner", "persist", "phasing", "poisonous", "populate", "proliferate", "raid", "rally", "rebound", "recover", "reinforce", "renown", "replicate", "revolt", "ripple", "sacrifice", "scavenge", "shadow", "soulbond", "soulshift", "spectacle", "splice", "split second", "storm", "sunburst", "support", "suspend", "totem armor", "transfigure", "transform", "transmute", "undying", "unearth", "vanishing", "venture", "wither", "absorb", "aftermath", "awaken", "blitz", "boast", "channel", "connive", "daybound", "disturb", "embolden", "encore", "energize", "fateful hour", "ferocious", "formidable", "foundry", "frenzy", "fuse", "hellbent", "heroic", "imprint", "inspire", "join forces", "kinship", "landship", "lieutenant", "metalcraft", "morbid", "parley", "radiance", "raid", "rally", "recover", "reinforce", "renown", "revolt", "spell mastery", "strive", "sweep", "tempting offer", "threshold", "undergrowth", "will of the council"];
-      for (let i = 0; i < card.text.length; i++) {
-        const words = card.text[i].toLowerCase().split(/\s+/);
-        if (!words.every(word => keywords.includes(word.toLocaleLowerCase()))) {
-          return false;
-        }
-      }
+      let text = card.text.join(" ").toLowerCase();
+      keywords.forEach(keyword => {
+        text = text.replace(keyword.toLocaleLowerCase(), "");
+      });
+      text = text.trim();
+      return text.length === 0;
     }
     return true;
   }
